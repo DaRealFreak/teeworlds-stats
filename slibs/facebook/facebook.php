363ec555a8639e19f5404d5eaf5198a035ba3a6d
Steffen Keuper <steffen.keuper@d-mind.de>
2017-11-07 15:07:05 +0100

    [TASK] add facebook sdk

--- slibs/facebook/facebook.php
+++ slibs/facebook/facebook.php
@@ -1,10 +1,27 @@
 <?php
 
-require_once("library/facebook.php");
+require_once("library/Facebook/autoload.php");
 
-$facebook = new Facebook(array(
-  'appId'  => '626460964035808',
-  'secret' => '',
+$facebook = new \Facebook\Facebook(array(
+    'app_id' => '133904623933394',
+    'app_secret' => '',
 ));
+
+
+try {
+    // Returns a `Facebook\FacebookResponse` object
+    $response = $facebook->get('/me?fields=id,name', '133904623933394|vcXEANJfGqkKCSzINDGXsIZHJh0');
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+}
+
+$user = $response->getGraphUser();
+
+echo 'Name: ' . $user['name'];
+
 
 ?>
