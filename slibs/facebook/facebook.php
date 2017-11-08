0fcc8f734bf5501d32fe7030056205e8019f41f2
Steffen Keuper <steffen.keuper@web.de>
2017-11-08 13:31:48 +0100

    [CLEANUP] remove test facebook user retrieval code

--- slibs/facebook/facebook.php
+++ slibs/facebook/facebook.php
@@ -7,21 +7,4 @@ $facebook = new \Facebook\Facebook(array(
     'app_secret' => '',
 ));
 
-
-try {
-    // Returns a `Facebook\FacebookResponse` object
-    $response = $facebook->get('/me?fields=id,name', '133904623933394|vcXEANJfGqkKCSzINDGXsIZHJh0');
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-    echo 'Graph returned an error: ' . $e->getMessage();
-    exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-    echo 'Facebook SDK returned an error: ' . $e->getMessage();
-    exit;
-}
-
-$user = $response->getGraphUser();
-
-echo 'Name: ' . $user['name'];
-
-
 ?>
