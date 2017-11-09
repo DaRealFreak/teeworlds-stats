486d627777542a5785c0c3631c3a8a3847806a90
Steffen Keuper <steffen.keuper@d-mind.de>
2017-11-09 10:19:04 +0100

    [TASK] update sql connection for docker structure

--- _cronjobs/config_sql.php
+++ _cronjobs/config_sql.php
@@ -1,19 +1,19 @@
 <?php
 
-$sql_host = "localhost";
+$sql_host = "db";
 $sql_user = "root";
-$sql_pass = "";
-$sql_db = "teeworlds";
+$sql_pass = "root";
+$sql_db = "teestats";
 
 
 try {
-	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-	$db = new PDO("mysql:host=$sql_host;dbname=$sql_db", $sql_user, $sql_pass, $pdo_options);
-}
-catch (PDOException $e) {
-		include("down.php");
-		@mail("contact@teeworlds-stats.info","Teeworlds stats : MySQL error",$e->getMessage());
-		exit(0);
+    $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+    $db = new PDO("mysql:host=$sql_host;dbname=$sql_db", $sql_user, $sql_pass, $pdo_options);
+} catch (PDOException $e) {
+    // ToDo: missing
+    include("down.php");
+    @mail("contact@teeworlds-stats.info", "Teeworlds stats : MySQL error", $e->getMessage());
+    exit(0);
 }
 
 unset($sql_host, $sql_user, $sql_pass, $sql_db);
