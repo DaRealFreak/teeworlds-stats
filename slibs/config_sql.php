fcfd7e1efb664518fe695d6fa194a4c6627b0900
Steffen Keuper <steffen.keuper@web.de>
2017-11-09 10:17:17 +0100

    [TASK] refactoring of try catch clause

--- slibs/config_sql.php
+++ slibs/config_sql.php
@@ -6,12 +6,11 @@ $sql_pass = "root";
 $sql_db = "teestats";
 
 try {
-	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-	$db = new PDO("mysql:host=$sql_host;dbname=$sql_db", $sql_user, $sql_pass, $pdo_options);
-}
-catch (PDOException $e) {
-		echo trender("templates/down.twig");
-		exit(0);
+    $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+    $db = new PDO("mysql:host=$sql_host;dbname=$sql_db", $sql_user, $sql_pass, $pdo_options);
+} catch (PDOException $e) {
+    echo trender("templates/down.twig");
+    exit(0);
 }
 
 unset($sql_host, $sql_user, $sql_pass, $sql_db);
