64114c5b2fea58eabe6f03f53a76a15ee9327877
Steffen Keuper <steffen.keuper@web.de>
2017-11-09 10:20:09 +0100

    [TASK] add uid and country code to saved servers

--- _cronjobs/gather_servers.php
+++ _cronjobs/gather_servers.php
@@ -1,8 +1,7 @@
 <?php
-
 $timeA = time();
 
-ini_set("max_execution_time", 10*60);
+ini_set("max_execution_time", 10 * 60);
 $curdate = date('Y-m-d H:i:s');
 
 include(dirname(__FILE__) . "/TwRequest.php");
@@ -17,10 +16,8 @@ include(dirname(__FILE__) . "/../slibs/config_sql.php");
 
 $db->query("DELETE FROM servers");
 
-$req = $db->prepare("INSERT INTO servers VALUES (?,?)");
+$req = $db->prepare("INSERT INTO servers VALUES (NULL, ?, ?, ?)");
 
-foreach($servers as $server)
-	$req->execute(array($server[0],$server[1]));
-
-
-?>
+foreach ($servers as $server) {
+    $req->execute(array($server[0], $server[1], $server[2]));
+}
