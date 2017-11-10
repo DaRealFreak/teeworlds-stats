11b9daf8c68c7b5fa58b5349cd7f338b27b12cbe
Steffen Keuper <steffen.keuper@web.de>
2017-11-10 23:46:17 +0100

    [TASK] add file to class mapping, define twstats namespace

--- web/core/Backend/RequestHandler.php
+++ web/core/Backend/RequestHandler.php
@@ -12,24 +12,43 @@ class RequestHandler
      *
      * @return string
      */
-    public static function getUrl() {
-        $scheme = $_SERVER['HTTP_X_FORWARDED_PROTO'] ? $_SERVER['HTTP_X_FORWARDED_PROTO']: $_SERVER['REQUEST_SCHEME'];
+    public static function getUrl()
+    {
+        $scheme = $_SERVER['HTTP_X_FORWARDED_PROTO'] ? $_SERVER['HTTP_X_FORWARDED_PROTO'] : $_SERVER['REQUEST_SCHEME'];
         return "$scheme://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     }
 
     /**
-     * retrieve the requested path
-     * if nothing is passed return empty string to execute the main action
+     * retrieve the path of the requested file in the twstats directory
+     * if nothing is passed or the file doesn't exist return the path to the main file
      *
      * @return mixed
      */
-    public static function getRequestedPath() {
+    public static function getRequestedPath()
+    {
         $requestedPath = substr(parse_url(self::getUrl(), PHP_URL_PATH), 1);
         if ($requestedPath) {
-            return GeneralUtility::joinPaths(TwStats_path, $requestedPath);
+            $requestedPath = GeneralUtility::joinPaths(TwStats_path, $requestedPath);
+            if (!is_file($requestedPath) || !is_file($requestedPath)) {
+                $requestedPath = TwStats_main;
+            }
         } else {
-            return $requestedPath;
+            return TwStats_main;
         }
+        return $requestedPath;
+    }
+
+    /**
+     * return the namespace of the corresponding class to the requested file
+     * as per the definitions in the \TwStats\Core\Backend\SystemEnvironmentBuilder
+     *
+     * @return string
+     */
+    public static function getRequestedClass()
+    {
+        $requestedPath = self::getRequestedPath();
+        $parsedPath = pathinfo($requestedPath);
+        return TwStats_namespace . ucfirst($parsedPath['filename']);
     }
 
     /**
@@ -37,7 +56,8 @@ class RequestHandler
      *
      * @return mixed
      */
-    public static function getArguments() {
+    public static function getArguments()
+    {
         return parse_url(self::getUrl(), PHP_URL_QUERY);
     }
 }
