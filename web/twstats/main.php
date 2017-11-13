ffc1c577bc93454aee6bbe2842cfd80233616f4e
Steffen Keuper <steffen.keuper@web.de>
2017-11-13 09:58:17 +0100

    [TASK] update main function to current project structure

--- web/twstats/main.php
+++ web/twstats/main.php
@@ -6,22 +6,42 @@ use TwStats\Core\Frontend\AbstractController;
 
 class Main extends AbstractController
 {
-    public function run()
+
+    /**
+     * ToDo: extract to url handler
+     *
+     * @param string $page
+     * @param array $params
+     * @param bool $usePath
+     * @return string
+     */
+    private function myurl($page = "", $params = array(), $usePath = true)
     {
-        $this->frontendHandler->renderTemplate("main.twig");
+        if (!$usePath) {
+            $params['p'] = $page;
+            $url = "index.php?" . http_build_query($params);
+        } else {
+            $url = $page;
+            if (!empty($params))
+                $url = $url . "?" . http_build_query($params);
+        }
+
+        return $url;
     }
 
-    public function originalFunction($twig) {
+    public function run()
+    {
         $items = array(
             array('text' => 'Game statistics',
-                'url' => myurl("general"),
+                'url' => $this->myurl("general"),
                 'class' => 'icon-globe'),
             array('text' => 'Search',
-                'url' => myurl(""),
+                'url' => $this->myurl(""),
                 'class' => 'icon-search')
         );
 
-        $user = getFacebookID();
+        // ToDo: implement Facebook SDK again
+        $user = 0; //getFacebookID();
 
         if ($user) {
             $page['logged'] = true;
@@ -29,40 +49,38 @@ class Main extends AbstractController
             $account = getAccountDetails($user);
             if (!empty($account["tee"]))
                 $items[] = array('text' => $account['tee'],
-                    'url' => myurl("tee", array("n" => $account['tee'])),
+                    'url' => $this->myurl("tee", array("n" => $account['tee'])),
                     'class' => 'icon-user');
             if (!empty($account["clan"]))
                 $items[] = array('text' => $account['clan'],
-                    'url' => myurl("clan", array("n" => $account['clan'])),
+                    'url' => $this->myurl("clan", array("n" => $account['clan'])),
                     'class' => 'icon-home');
 
-            $items[] = array('text' => 'Account', 'url' => myurl("account"), 'class' => 'icon-pencil');
+            $items[] = array('text' => 'Account', 'url' => $this->myurl("account"), 'class' => 'icon-pencil');
         }
 
-        $items[] = array('text' => 'About', 'url' => myurl("about"), 'class' => 'icon-info-sign');
+        $items[] = array('text' => 'About', 'url' => $this->myurl("about"), 'class' => 'icon-info-sign');
 
-        $page['navigation'] = $twig->render("views/navigation.twig", array("items" => $items));
+        $page['navigation'] = $this->frontendHandler->getTemplateHtml("views/navigation.twig", array("items" => $items));
 
-        if (!empty($_SESSION['suggestionsTee']))
-            $page["suggestionsTee"] = $_SESSION['suggestionsTee'];
-        if (!empty($_SESSION['suggestionsClan']))
-            $page["suggestionsClan"] = $_SESSION['suggestionsClan'];
-        if (!empty($_SESSION['suggestionsServer']))
-            $page["suggestionsServer"] = $_SESSION['suggestionsServer'];
-        if (!empty($_SESSION['missingTee']))
-            $page["missingTee"] = $_SESSION['missingTee'];
-        if (!empty($_SESSION['missingClan']))
-            $page["missingClan"] = $_SESSION['missingClan'];
-        if (!empty($_SESSION['missingServer']))
-            $page["missingServer"] = $_SESSION['missingServer'];
+        /*
+         * ToDo: change session arguments to actual post arguments
+         */
+        $options = [
+            'suggestionsTee',
+            'suggestionsClan',
+            'suggestionsServer',
+            'missingTee',
+            'missingClan',
+            'missingServer'
+        ];
+        foreach ($options as $argument) {
+            if ($this->sessionHandler->hasArgument($argument)) {
+                $page[$argument] = $this->sessionHandler->getArgument($argument);
+                $this->sessionHandler->removeAgument($argument);
+            }
+        }
 
-        unset($_SESSION['suggestionsClan'],
-            $_SESSION['suggestionsServer'],
-            $_SESSION['suggestionsTee'],
-            $_SESSION['missingTee'],
-            $_SESSION['missingClan'],
-            $_SESSION['missingServer']);
-
-        echo trender("templates/main.twig", $page);
+        $this->frontendHandler->renderTemplate("main.twig", $page);
     }
 }
