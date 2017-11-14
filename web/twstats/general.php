c6130dc796addf0b43863fd3b87acd76b10cb593
Steffen Keuper <steffen.keuper@web.de>
2017-11-14 10:06:03 +0100

    [TASK] reimplement general

--- web/twstats/general.php
+++ web/twstats/general.php
@@ -1,64 +1,156 @@
 <?php
 
-$user = getFacebookID();
-
-if($user)
-	$page['logged'] = true;
-else
-	$page['logged'] = false;
+namespace TwStats\Ext;
 
 
-$items = array(
-			   array('text' => 'Game statistics',
-					 'url'=>myurl("general"),
-					 'class' => 'icon-globe'),
-			   array('text' => 'Search',
-					 'url'=>myurl(""),
-					 'class' => 'icon-search')
-			   );
+use TwStats\Core\Frontend\AbstractController;
+use TwStats\Core\Utility\GeneralUtility;
+use TwStats\Ext\Database\StatRepository;
+use TwStats\Ext\Facebook\Facebook;
 
-$user = getFacebookID();
+class General extends AbstractController
+{
 
-if($user) {
-	$page['logged'] = true;
+    /**
+     * @var Facebook|null
+     */
+    protected $facebook = null;
 
-	$account = getAccountDetails($user);
-	if(!empty($account["tee"]))
-		$items[] = array('text' => $account['tee'],
-						 'url'=>myurl("tee",array("n" => $account['tee'])),
-						 'class' => 'icon-user');
-	if(!empty($account["clan"]))
-		$items[] = array('text' => $account['clan'],
-						 'url'=>myurl("clan",array("n" => $account['clan'])),
-						 'class' => 'icon-home');
+    /**
+     * @var StatRepository
+     */
+    protected $statRepository = null;
 
-	$items[] = array('text' => 'Account', 'url' => myurl("account"), 'class' => 'icon-pencil');
+    /**
+     * initializing function to replace the constructor function
+     */
+    public function initialize()
+    {
+        $this->facebook = GeneralUtility::makeInstance(Facebook::class);
+        $this->statRepository = GeneralUtility::makeInstance(StatRepository::class);
+    }
+
+    /**
+     * Starting point
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $items = array(
+            array('text' => 'Game statistics',
+                'url' => $this->prettyUrl->buildPrettyUri("general"),
+                'class' => 'icon-globe'),
+            array('text' => 'Search',
+                'url' => $this->prettyUrl->buildPrettyUri(""),
+                'class' => 'icon-search')
+        );
+
+        $user = $this->facebook->getFacebookID();
+
+        if ($user) {
+            $page['logged'] = true;
+
+            $account = $this->facebook->getAccountDetails($user);
+            if (!empty($account["tee"])) {
+                $items[] = array('text' => $account['tee'],
+                    'url' => $this->prettyUrl->buildPrettyUri("tee", array("n" => $account['tee'])),
+                    'class' => 'icon-user');
+            }
+            if (!empty($account["clan"])) {
+                $items[] = array('text' => $account['clan'],
+                    'url' => $this->prettyUrl->buildPrettyUri("clan", array("n" => $account['clan'])),
+                    'class' => 'icon-home');
+            }
+
+            $items[] = array('text' => 'Account', 'url' => $this->prettyUrl->buildPrettyUri("account"), 'class' => 'icon-pencil');
+        } else {
+            $page['logged'] = false;
+        }
+
+        $items[] = array('text' => 'About', 'url' => $this->prettyUrl->buildPrettyUri("about"), 'class' => 'icon-info-sign');
+
+        $page['navigation'] = $this->frontendHandler->getTemplateHtml("views/navigation.twig", array("items" => $items));
+
+        // Stats
+        $histogramMod = $this->statRepository->getglobalhisto("mod", 13);
+        $histogramCountry = $this->statRepository->getglobalhisto("country", 13);
+
+        $page['mods'] = $this->frontendHandler->getTemplateHtml("views/pie.twig",
+            array("id" => "piemods",
+                "name" => "Most played mods",
+                "histogram" => $histogramMod));
+
+        $page['countries'] = $this->frontendHandler->getTemplateHtml("views/pie.twig",
+            array("id" => "piecountries",
+                "name" => "Most playing countries",
+                "histogram" => $histogramCountry));
+
+        $page += $this->statRepository->generalCounts();
+
+        $this->frontendHandler->renderTemplate("general.twig", $page);
+    }
+
+    public function old_run()
+    {
+        $user = getFacebookID();
+
+        if ($user)
+            $page['logged'] = true;
+        else
+            $page['logged'] = false;
+
+
+        $items = array(
+            array('text' => 'Game statistics',
+                'url' => myurl("general"),
+                'class' => 'icon-globe'),
+            array('text' => 'Search',
+                'url' => myurl(""),
+                'class' => 'icon-search')
+        );
+
+        $user = getFacebookID();
+
+        if ($user) {
+            $page['logged'] = true;
+
+            $account = getAccountDetails($user);
+            if (!empty($account["tee"]))
+                $items[] = array('text' => $account['tee'],
+                    'url' => myurl("tee", array("n" => $account['tee'])),
+                    'class' => 'icon-user');
+            if (!empty($account["clan"]))
+                $items[] = array('text' => $account['clan'],
+                    'url' => myurl("clan", array("n" => $account['clan'])),
+                    'class' => 'icon-home');
+
+            $items[] = array('text' => 'Account', 'url' => myurl("account"), 'class' => 'icon-pencil');
+        } else
+            $page['logged'] = false;
+
+        $items[] = array('text' => 'About', 'url' => myurl("about"), 'class' => 'icon-info-sign');
+
+        $page['navigation'] = $twig->render("views/navigation.twig", array("items" => $items));
+
+
+        // Stats
+        $hmod = getglobalhisto("mod", 13);
+        $hcountry = getglobalhisto("country", 13);
+
+
+        $page['mods'] = $twig->render("views/pie.twig",
+            array("id" => "piemods",
+                "name" => "Most played mods",
+                "histogram" => $hmod));
+
+        $page['countries'] = $twig->render("views/pie.twig",
+            array("id" => "piecountries",
+                "name" => "Most playing countries",
+                "histogram" => $hcountry));
+
+        $page += generalCounts();
+
+        echo trender("templates/general.twig", $page);
+    }
 }
-else
-	$page['logged'] = false;
-
-$items[] = array('text' => 'About', 'url'=>myurl("about"), 'class' => 'icon-info-sign');
-
-$page['navigation'] = $twig->render("views/navigation.twig", array("items" => $items));
-
-
-// Stats
-$hmod = getglobalhisto("mod",13);
-$hcountry = getglobalhisto("country",13);
-
-
-$page['mods'] = $twig->render("views/pie.twig",
-								  array("id" => "piemods",
-										"name" => "Most played mods",
-										"histogram" => $hmod));
-
-$page['countries'] = $twig->render("views/pie.twig",
-								  array("id" => "piecountries",
-										"name" => "Most playing countries",
-										"histogram" => $hcountry));
-
-$page += generalCounts();
-
-echo trender("templates/general.twig", $page);
-
-?>
