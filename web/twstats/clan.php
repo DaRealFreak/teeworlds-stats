4320ac7de8b7a7ec9d9d935319ad70988f863f15
Steffen Keuper <steffen.keuper@web.de>
2017-11-14 17:37:31 +0100

    [TASK] reimplement clan and server, remove old functions, remove slibs

--- web/twstats/clan.php
+++ web/twstats/clan.php
@@ -1,103 +1,144 @@
 <?php
 
-$clan = empty($uri[1]) ? '' : $uri[1];
-$clan = empty($gp['n']) ? $clan : $gp['n'];
+namespace TwStats\Ext;
 
-if(empty($clan))
-	redirect(".");
 
-$name = getClanName($clan);
-if(!$name) {
-	$_SESSION['suggestionsClan'] = getSimilarData("clan", $clan);
-	$_SESSION['missingClan'] = true;
-	redirect(".");
+use TwStats\Core\Frontend\AbstractController;
+use TwStats\Core\Utility\GeneralUtility;
+use TwStats\Ext\Database\AccountRepository;
+use TwStats\Ext\Database\StatRepository;
+use TwStats\Ext\Facebook\Facebook;
+use TwStats\Ext\Youtube\Youtube;
+
+class Clan extends AbstractController
+{
+
+    /**
+     * @var Facebook
+     */
+    protected $facebook = null;
+
+    /**
+     * @var AccountRepository
+     */
+    protected $accountRepository = null;
+
+    /**
+     * @var StatRepository
+     */
+    protected $statRepository = null;
+
+    /**
+     * initializing function to replace the constructor function
+     */
+    public function initialize()
+    {
+        $this->facebook = GeneralUtility::makeInstance(Facebook::class);
+        $this->accountRepository = GeneralUtility::makeInstance(AccountRepository::class);
+        $this->statRepository = GeneralUtility::makeInstance(StatRepository::class);
+    }
+
+    /**
+     * Starting point
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // ToDo: argument parsing from url
+        $clan = 'OnFire';
+        if (empty($clan))
+            GeneralUtility::redirectToUri(".");
+
+        $name = $this->statRepository->getClanName($clan);
+        if (!$name) {
+            $_SESSION['suggestionsClan'] = $this->statRepository->getSimilarData("clan", $clan);
+            $_SESSION['missingClan'] = true;
+            GeneralUtility::redirectToUri(".");
+        }
+        $clan = $name;
+
+        $items = array(
+            array('text' => 'Game statistics',
+                'url' => $this->prettyUrl->buildPrettyUri("general"),
+                'class' => 'icon-globe'),
+            array('text' => 'Search',
+                'url' => $this->prettyUrl->buildPrettyUri(""),
+                'class' => 'icon-search')
+        );
+
+        $user = $this->facebook->getFacebookID();
+        if ($user) {
+            $page['logged'] = true;
+
+            $account = $this->facebook->getAccountDetails($user);
+            if (!empty($account["tee"]))
+                $items[] = array('text' => $account['tee'],
+                    'url' => $this->prettyUrl->buildPrettyUri("tee", array("n" => $account['tee'])),
+                    'class' => 'icon-user');
+            if (!empty($account["clan"]))
+                $items[] = array('text' => $account['clan'],
+                    'url' => $this->prettyUrl->buildPrettyUri("clan", array("n" => $account['clan'])),
+                    'class' => 'icon-home');
+
+            $items[] = array('text' => 'Account', 'url' => $this->prettyUrl->buildPrettyUri("account"), 'class' => 'icon-pencil');
+        }
+
+        $items[] = array('text' => 'About', 'url' => $this->prettyUrl->buildPrettyUri("about"), 'class' => 'icon-info-sign');
+
+        $page['navigation'] = $this->frontendHandler->getTemplateHtml("views/navigation.twig", array("items" => $items));
+
+        /*		SELECTING CLAN INFO TO DISPLAY		*/
+        $clanDetails = $this->accountRepository->getClanDetails($clan);
+
+        if (!empty($clanDetails["clantxt"]))
+            if (strip_tags($clanDetails["clantxt"]) != "")
+                $page["clantxt"] = Youtube::integrateYoutubeVideos($clanDetails["clantxt"]);
+
+        if ($clanDetails["clanmods"] == 1) {
+            $hist_mods = $this->statRepository->gethisto("clan", $clan, "mod");
+            $page['mods'] = $this->frontendHandler->getTemplateHtml("views/pie.twig", array("histogram" => $hist_mods,
+                "name" => "$clan mods",
+                "id" => "piemods"));
+        }
+
+        if ($clanDetails["clanmaps"] == 1) {
+            $hist_maps = $this->statRepository->gethisto("clan", $clan, "map");
+            $page['maps'] = $this->frontendHandler->getTemplateHtml("views/pie.twig", array("histogram" => $hist_maps,
+                "name" => "$clan maps",
+                "id" => "piemaps"));
+        }
+
+
+        if ($clanDetails["clancountries"] == 1) {
+            $hist_countries = $this->statRepository->gethisto("clan", $clan, "country");
+            $page['countries'] = $this->frontendHandler->getTemplateHtml("views/pie.twig", array("histogram" => $hist_countries,
+                "name" => "$clan countries",
+                "id" => "piecountries"));
+        }
+
+        if ($clanDetails["clanhours"] == 1) {
+            $hhours = $this->statRepository->gethours("clan", $clan);
+            $page['hours'] = $this->frontendHandler->getTemplateHtml("views/bars.twig", array("histogram" => $hhours,
+                "name" => "$clan online time per hour",
+                "id" => "piehours"));
+        }
+
+        if ($clanDetails["clandays"] == 1) {
+            $hdays = $this->statRepository->getdays("clan", $clan);
+            $page['days'] = $this->frontendHandler->getTemplateHtml("views/bars.twig", array("histogram" => $hdays,
+                "name" => "$clan online time per day (Monday to Sunday)",
+                "id" => "piedays"));
+        }
+
+        if ($clanDetails["clanplayers"] == 1) {
+            $players = $this->statRepository->getClanPlayers($clan);
+            $page['players'] = $this->frontendHandler->getTemplateHtml("views/playerlist.twig", array("title" => "$clan players",
+                "players" => $players));
+        }
+        $page['title'] = "$clan statistics on Teeworlds";
+        $page['clan'] = $clan;
+
+        $this->frontendHandler->renderTemplate("templates/clan.twig", $page);
+    }
 }
-$clan = $name;
-
-$items = array(
-			   array('text' => 'Game statistics',
-					 'url'=>myurl("general"),
-					 'class' => 'icon-globe'),
-			   array('text' => 'Search',
-					 'url'=>myurl(""),
-					 'class' => 'icon-search')
-			   );
-
-$user = getFacebookID();
-if($user) {
-	$page['logged'] = true;
-
-	$account = getAccountDetails($user);
-	if(!empty($account["tee"]))
-		$items[] = array('text' => $account['tee'],
-						 'url'=>myurl("tee",array("n" => $account['tee'])),
-						 'class' => 'icon-user');
-	if(!empty($account["clan"]))
-		$items[] = array('text' => $account['clan'],
-						 'url'=>myurl("clan",array("n" => $account['clan'])),
-						 'class' => 'icon-home');
-
-	$items[] = array('text' => 'Account', 'url' => myurl("account"), 'class' => 'icon-pencil');
-}
-
-$items[] = array('text' => 'About', 'url'=>myurl("about"), 'class' => 'icon-info-sign');
-
-$page['navigation'] = $twig->render("views/navigation.twig", array( "items" => $items));
-
-/*		SELECTING CLAN INFO TO DISPLAY		*/
-
-
-$clanDetails = getClanDetails($clan);
-
-if(!empty($clanDetails["clantxt"]))
-	if(strip_tags($clanDetails["clantxt"]) != "")
-		$page["clantxt"] = integrateYoutubeVideos($clanDetails["clantxt"]);
-
-if($clanDetails["clanmods"] == 1) {
-	$hist_mods = gethisto("clan",$clan,"mod");
-	$page['mods'] = $twig->render("views/pie.twig", array("histogram" => $hist_mods,
-																   "name" => "$clan mods",
-																   "id" => "piemods"));
-}
-
-if($clanDetails["clanmaps"] == 1) {
-	$hist_maps = gethisto("clan",$clan,"map");
-	$page['maps'] = $twig->render("views/pie.twig", array("histogram" => $hist_maps,
-																   "name" => "$clan maps",
-																   "id" => "piemaps"));
-}
-
-
-if($clanDetails["clancountries"] == 1) {
-	$hist_countries = gethisto("clan",$clan,"country");
-	$page['countries'] = $twig->render("views/pie.twig", array("histogram" => $hist_countries,
-																   "name" => "$clan countries",
-																   "id" => "piecountries"));
-}
-
-if($clanDetails["clanhours"] == 1) {
-	$hhours = gethours("clan",$clan);
-	$page['hours'] = $twig->render("views/bars.twig", array("histogram" => $hhours,
-																   "name" => "$clan online time per hour",
-																   "id" => "piehours"));
-}
-
-if($clanDetails["clandays"] == 1) {
-	$hdays = getdays("clan", $clan);
-	$page['days'] = $twig->render("views/bars.twig", array("histogram" => $hdays,
-																   "name" => "$clan online time per day (Monday to Sunday)",
-																   "id" => "piedays"));
-}
-
-if($clanDetails["clanplayers"] == 1) {
-	$players = getClanPlayers($clan);
-	$page['players'] = $twig->render("views/playerlist.twig", array("title" => "$clan players",
-																"players" => $players));
-}
-
-
-$page['title'] = "$clan statistics on Teeworlds";
-$page['clan'] = $clan;
-
-
-echo trender("templates/clan.twig", $page);
--- web/twstats/server.php
+++ web/twstats/server.php
@@ -1,81 +1,125 @@
 <?php
 
-$server = empty($uri[1]) ? '' : $uri[1];
-$server = empty($gp['n']) ? $server : $gp['n'];
+namespace TwStats\Ext;
 
-if(empty($server))
-	redirect(".");
 
-$name = getServerName($server);
+use TwStats\Core\Frontend\AbstractController;
+use TwStats\Core\Utility\GeneralUtility;
+use TwStats\Ext\Database\AccountRepository;
+use TwStats\Ext\Database\StatRepository;
+use TwStats\Ext\Facebook\Facebook;
 
-if(!$name) {
-	$_SESSION['suggestionsServer'] = getSimilarData("server", $server);
-	$_SESSION['missingServer'] = true;
-	redirect(".");
+class Server extends AbstractController
+{
+
+    /**
+     * @var Facebook
+     */
+    protected $facebook = null;
+
+    /**
+     * @var AccountRepository
+     */
+    protected $accountRepository = null;
+
+    /**
+     * @var StatRepository
+     */
+    protected $statRepository = null;
+
+    /**
+     * initializing function to replace the constructor function
+     */
+    public function initialize()
+    {
+        $this->facebook = GeneralUtility::makeInstance(Facebook::class);
+        $this->accountRepository = GeneralUtility::makeInstance(AccountRepository::class);
+        $this->statRepository = GeneralUtility::makeInstance(StatRepository::class);
+    }
+
+    /**
+     * Starting point
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // ToDo: argument parsing from url
+        $server = 'DDraceNetwork';
+
+        if (empty($server))
+            GeneralUtility::redirectToUri(".");
+
+        $name = $this->statRepository->getServerName($server);
+
+        if (!$name) {
+            $_SESSION['suggestionsServer'] = $this->statRepository->getSimilarData("server", $server);
+            $_SESSION['missingServer'] = true;
+            GeneralUtility::redirectToUri(".");
+        }
+        $server = $name;
+
+
+        $user = $this->facebook->getFacebookID();
+
+        if ($user) {
+            $page['logged'] = true;
+
+            $account = $this->facebook->getAccountDetails($user);
+            if (!empty($account["tee"]))
+                $items[] = array('text' => $account['tee'],
+                    'url' => $this->prettyUrl->buildPrettyUri("tee", array("n" => $account['tee'])),
+                    'class' => 'icon-user');
+            if (!empty($account["clan"]))
+                $items[] = array('text' => $account['clan'],
+                    'url' => $this->prettyUrl->buildPrettyUri("clan", array("n" => $account['clan'])),
+                    'class' => 'icon-home');
+
+            $items[] = array('text' => 'Account', 'url' => $this->prettyUrl->buildPrettyUri("account"), 'class' => 'icon-pencil');
+        }
+
+        $hist_maps = $this->statRepository->gethisto("server", $server, "map");
+        $hist_countries = $this->statRepository->gethisto("server", $server, "country");
+
+        $hhours = $this->statRepository->gethours("server", $server);
+        $hdays = $this->statRepository->getdays("server", $server);
+
+
+        $items = array(
+            array('text' => 'Game statistics',
+                'url' => $this->prettyUrl->buildPrettyUri("general"),
+                'class' => 'graphs'),
+            array('text' => 'Search',
+                'url' => $this->prettyUrl->buildPrettyUri(""),
+                'class' => 'gallery'),
+            array('text' => 'About',
+                'url' => $this->prettyUrl->buildPrettyUri("about"),
+                'class' => 'typo')
+        );
+
+        $page['navigation'] = $this->frontendHandler->getTemplateHtml("views/navigation.twig", array("items" => $items));
+
+        $players = $this->statRepository->getServerPlayers($server);
+
+        $page['title'] = "$server statistics on Teeworlds";
+        $page['server'] = $server;
+
+        $page['players'] = $this->frontendHandler->getTemplateHtml("views/playerlist.twig", array("title" => "Playing tees",
+            "players" => $players));
+
+        $page['countries'] = $this->frontendHandler->getTemplateHtml("views/pie.twig", array("histogram" => $hist_countries,
+            "name" => "Most playing countries",
+            "id" => "piecountries"));
+        $page['maps'] = $this->frontendHandler->getTemplateHtml("views/pie.twig", array("histogram" => $hist_maps,
+            "name" => "Most played maps	",
+            "id" => "piemaps"));
+        $page['hours'] = $this->frontendHandler->getTemplateHtml("views/bars.twig", array("histogram" => $hhours,
+            "name" => "Online time per hour",
+            "id" => "piehours"));
+        $page['days'] = $this->frontendHandler->getTemplateHtml("views/bars.twig", array("histogram" => $hdays,
+            "name" => "Online time per day (Monday to Sunday)",
+            "id" => "piedays"));
+
+        $this->frontendHandler->renderTemplate("server.twig", $page);
+    }
 }
-$server = $name;
-
-
-$user = getFacebookID();
-
-if($user) {
-	$page['logged'] = true;
-
-	$account = getAccountDetails($user);
-	if(!empty($account["tee"]))
-		$items[] = array('text' => $account['tee'],
-						 'url'=>myurl("tee",array("n" => $account['tee'])),
-						 'class' => 'icon-user');
-	if(!empty($account["clan"]))
-		$items[] = array('text' => $account['clan'],
-						 'url'=>myurl("clan",array("n" => $account['clan'])),
-						 'class' => 'icon-home');
-
-	$items[] = array('text' => 'Account', 'url' => myurl("account"), 'class' => 'icon-pencil');
-}
-
-$hist_maps = gethisto("server",$server,"map");
-$hist_countries = gethisto("server",$server,"country");
-
-$hhours = gethours("server",$server);
-$hdays = getdays("server", $server);
-
-
-$items = array(
-			   array('text' => 'Game statistics',
-					 'url'=>myurl("general"),
-					 'class' => 'graphs'),
-			   array('text' => 'Search',
-					 'url'=>myurl(""),
-					 'class' => 'gallery'),
-			   array('text' => 'About',
-					 'url'=>myurl("about"),
-					 'class' => 'typo')
-			   );
-
-$page['navigation'] = $twig->render("views/navigation.twig", array( "items" => $items));
-
-$players = getServerPlayers($server);
-
-$page['title'] = "$server statistics on Teeworlds";
-$page['server'] = $server;
-
-$page['players'] = $twig->render("views/playerlist.twig", array("title" => "Playing tees",
-															"players" => $players));
-
-$page['countries'] = $twig->render("views/pie.twig", array("histogram" => $hist_countries,
-															   "name" => "Most playing countries",
-															   "id" => "piecountries"));
-$page['maps'] = $twig->render("views/pie.twig", array("histogram" => $hist_maps,
-															   "name" => "Most played maps	",
-															   "id" => "piemaps"));
-$page['hours'] = $twig->render("views/bars.twig", array("histogram" => $hhours,
-															   "name" => "Online time per hour",
-															   "id" => "piehours"));
-$page['days'] = $twig->render("views/bars.twig", array("histogram" => $hdays,
-															   "name" => "Online time per day (Monday to Sunday)",
-															   "id" => "piedays"));
-
-echo trender("templates/server.twig", $page);
-
-?>
