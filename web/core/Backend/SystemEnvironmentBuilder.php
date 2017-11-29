eb28af497e8fe76ec8a9db0361ca1d5e2f69859a
Steffen Keuper <steffen.keuper@web.de>
2017-11-29 11:05:12 +0100

    [TASK] use realpath for constants

--- web/core/Backend/SystemEnvironmentBuilder.php
+++ web/core/Backend/SystemEnvironmentBuilder.php
@@ -31,14 +31,14 @@ class SystemEnvironmentBuilder
             $baseDir = dirname($_SERVER['PHP_SELF']);
         }
 
-        define('TwStats_root', GeneralUtility::joinPaths($baseDir, ".."));
-        define('TwStats_path', GeneralUtility::joinPaths($baseDir, "twstats"));
+        define('TwStats_root', realpath(GeneralUtility::joinPaths($baseDir, "..")));
+        define('TwStats_path', realpath(GeneralUtility::joinPaths($baseDir, "twstats")));
         define('TwStats_main_class', "main");
-        define('TwStats_main_file', GeneralUtility::joinPaths(TwStats_path, TwStats_main_class . ".php"));
+        define('TwStats_main_file', realpath(GeneralUtility::joinPaths(TwStats_path, TwStats_main_class . ".php")));
         define('TwStats_Ext_namespace', "TwStats\\Ext\\");
         define('TwStats_Cron_namespace', "TwStats\\Cron\\");
         define('TwStats_Core_namespace', "TwStats\\Core\\");
-        define('TwStats_templates', GeneralUtility::joinPaths($baseDir, "templates"));
-        define('TwStats_template_cache', GeneralUtility::joinPaths(TwStats_templates, "cache"));
+        define('TwStats_templates', realpath(GeneralUtility::joinPaths($baseDir, "templates")));
+        define('TwStats_template_cache', realpath(GeneralUtility::joinPaths(TwStats_templates, "cache")));
     }
 }
