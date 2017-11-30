bc2cef6385d35d88801bd562bd57e96cd5c30a5d
Steffen Keuper <steffen.keuper@web.de>
2017-11-30 12:37:23 +0100

    [TASK] change filter to function, implement slugifyUrl

--- web/twstats/twigextension/SlugifyUrl.php
+++ web/twstats/twigextension/SlugifyUrl.php
@@ -3,30 +3,30 @@
 namespace TwStats\Ext\TwigExtension;
 
 
+use TwStats\Core\Backend\RequestHandler;
 use TwStats\Core\General\SingletonInterface;
+use TwStats\Core\Utility\PrettyUrl;
 
 class SlugifyUrl extends \Twig_Extension implements SingletonInterface
 {
     /**
-     * default twig function to map filter to function
-     *
      * @return array
      */
-    public function getFilters()
+    public function getFunctions()
     {
         return array(
-            new \Twig_SimpleFilter('slugify', array($this, 'slugifyUrl')),
+            new \Twig_Function('slugify', array($this, 'slugifyUrl')),
         );
     }
 
     /**
      * ToDo: implement this function
-     *
-     * @param $currentUrl
-     * @return string
      */
-    public function slugifyUrl($currentUrl)
+    public function slugifyUrl()
     {
-        return $currentUrl . " is now pretty";
+        $class = strtok(RequestHandler::getUrl(), "?");
+        $url = "/" . PrettyUrl::buildPrettyUri($class, $_GET);
+        $title = "Title";
+        echo '<script>window.history.pushState("' . $url . '", "' . $title . '", "' . $url . '");</script>';
     }
 }
