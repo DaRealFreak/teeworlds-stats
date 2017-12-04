b2904e553ebe35105eff5f674835553e3ced3e34
Steffen Keuper <steffen.keuper@web.de>
2017-12-04 08:56:48 +0100

    [TASK] remove charisma libs, use custom asset instead

--- web/core/Frontend/Twig.php
+++ web/core/Frontend/Twig.php
@@ -48,7 +48,7 @@ class Twig implements SingletonInterface
         $this->settingManager = GeneralUtility::makeInstance(SettingManager::class);
         $this->htmlCompressor = Factory::construct();
 
-        $this->dependencies = $this->includeCharismaLibs();
+        $this->dependencies = $this->includeDependencies();
 
         $loader = new \Twig_Loader_Filesystem(TwStats_templates);
         $this->twig = new \Twig_Environment($loader, array(
@@ -126,7 +126,7 @@ class Twig implements SingletonInterface
      *
      * @return array
      */
-    private function includeCharismaLibs()
+    private function includeDependencies()
     {
         $baseDir = dirname($_SERVER['PHP_SELF']);
 
@@ -139,50 +139,10 @@ class Twig implements SingletonInterface
         }
 
         $res = ["basedir" => $baseDir];
-        $dir = GeneralUtility::joinPaths($baseDir, "clibs");
-        $res += $this->resolveCharismaLibs($dir);
-        return $res;
-    }
 
-    /**
-     * create a file list of the dependencies recursively
-     *
-     * ToDo:
-     *  - update charisma(current version is old) or remove it?
-     *
-     * @param $dir
-     * @return array
-     */
-    private function resolveCharismaLibs($dir)
-    {
-        $res = array("js" => array(), "css" => array());
-
-        if (file_exists($dir . "/dependencies.conf")) {
-            foreach (explode("\n", file_get_contents($dir . "/dependencies.conf")) as $fd) {
-                $fd = trim($fd);
-                if (file_exists("$dir/$fd")) {
-                    if (is_dir("$dir/$fd") && !empty($fd)) {
-                        $subres = $this->resolveCharismaLibs("$dir/$fd");
-                        $res['css'] = array_merge($res['css'], $subres['css']);
-                        $res['js'] = array_merge($res['js'], $subres['js']);
-                    } elseif (is_file("$dir/$fd")) {
-                        if (preg_match("/.*\.css/", $fd)) {
-                            $res['css'][] = "$dir/$fd";
-                        } elseif (preg_match("/.*\.js/", $fd)) {
-                            $res['js'][] = "$dir/$fd";
-                        }
-                    }
-                }
-            }
-        } else {
-            $res['js'] = array_merge($res['js'], glob("$dir/*.js"));
-            $res['css'] = array_merge($res['css'], glob("$dir/*.css"));
-            foreach (glob("$dir/*", GLOB_ONLYDIR) as $d) {
-                $subres = $this->resolveCharismaLibs($d);
-                $res['css'] = array_merge($res['css'], $subres['css']);
-                $res['js'] = array_merge($res['js'], $subres['js']);
-            }
-        }
+        // ToDo: extract these out of the code
+        $res['css'] = ['assets/twstats/public/css/bundle/bundle.min.css'];
+        $res['js'] = ['assets/twstats/public/js/bundle/bundle.js'];
         return $res;
     }
 
