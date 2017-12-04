140e2e9178508905bc37d66a8fe8dd5678c7d871
Steffen Keuper <steffen.keuper@web.de>
2017-12-04 09:23:13 +0100

    [TASK] ignore ssl verification for local development

--- web/core/Utility/GeneralUtility.php
+++ web/core/Utility/GeneralUtility.php
@@ -201,6 +201,10 @@ class GeneralUtility
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
+        // ToDo: extract to settings, verifying ssl should always be true
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
         $resp = curl_exec($ch);
         if (curl_errno($ch)) {
             throw new \Exception("Error loading '$url', " . curl_errno($ch));
