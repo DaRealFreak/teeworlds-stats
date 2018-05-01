abc6c02a1c3f062c91df985efff147df3da491f0
Steffen Keuper <steffen.keuper@web.de>
2018-05-01 22:30:06 +0200

    [TASK] remove player maps and player mods seeder from default seeding, generate complete player profiles on seeding instead

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,9 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ClansTableSeeder::class,
             PlayersTableSeeder::class,
-            PlayerStatusesTableSeeder::class,
-            PlayerMapsTableSeeder::class,
-            PlayerModsTableSeeder::class,
             ServersTableSeeder::class,
             ServerStatusesTableSeeder::class,
             ServerMapsTableSeeder::class,
--- database/seeds/PlayersTableSeeder.php
+++ database/seeds/PlayersTableSeeder.php
@@ -14,6 +14,11 @@ class PlayersTableSeeder extends Seeder
         // 25 players without clan,
         // ClansTableSeeder will create players in the clans too,
         // PlayerStatusesTableSeeder will create players with create Players with Status Records
-        factory(App\Models\Player::class, 25)->create();
+        factory(App\Models\Player::class, 25)->create()->each(function ($player) {
+            /** @var \App\Models\Player $player */
+            $player->maps()->save(factory(App\Models\PlayerMaps::class)->make());
+            $player->maps()->save(factory(App\Models\PlayerMods::class)->make());
+            $player->maps()->save(factory(App\Models\PlayerStatus::class)->make());
+        });
     }
 }
