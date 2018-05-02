13975643d1d07022f78923e61bc68c3495962bb2
Steffen Keuper <steffen.keuper@web.de>
2018-05-02 13:09:59 +0200

    [TASK] add function to retrieve online hours and days

--- app/Models/PlayerStatus.php
+++ app/Models/PlayerStatus.php
@@ -14,6 +14,32 @@ class PlayerStatus extends Model
 {
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
+    /**
+     * @return \Generator
+     */
+    public function onlineHours()
+    {
+        for ($i = 0; $i <= 23; $i++) {
+            yield $this->getAttribute('hour_' . $i);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function onlineDays()
+    {
+        return [
+            $this->getAttribute('monday'),
+            $this->getAttribute('tuesday'),
+            $this->getAttribute('wednesday'),
+            $this->getAttribute('thursday'),
+            $this->getAttribute('friday'),
+            $this->getAttribute('saturday'),
+            $this->getAttribute('sunday'),
+        ];
+    }
+
     public function player()
     {
         return $this->belongsTo(Player::class);
