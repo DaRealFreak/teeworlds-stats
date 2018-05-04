b815c85faec2a98c43d13b8571c96873546a0bcb
Steffen Keuper <steffen.keuper@web.de>
2018-05-04 23:34:06 +0200

    [BUGFIX] fix typo in used variable

--- resources/views/server.blade.php
+++ resources/views/server.blade.php
@@ -4,12 +4,12 @@
     <!-- Page Header-->
     <div class="page-header no-margin-bottom">
         <div class="container-fluid">
-            <h2 class="h5 no-margin-bottom">{{ $sever->name }}'s page</h2>
+            <h2 class="h5 no-margin-bottom">{{ $server->name }}'s page</h2>
         </div>
     </div>
     <section class="section-content">
         <div class="container-fluid">
-            @if ($sever->players)
+            @if ($server->players)
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="messages-block block">
@@ -40,7 +40,7 @@
             <div class="row">
                 <div class="col-lg-6">
                     <div class="pie-chart chart block">
-                        <div class="title"><strong>{{ $sever->name }}'s player maps</strong></div>
+                        <div class="title"><strong>{{ $server->name }}'s player maps</strong></div>
                         <div class="radar-chart chart margin-bottom-sm">
                             <canvas id="serverMaps"></canvas>
                         </div>
@@ -48,7 +48,7 @@
                 </div>
                 <div class="col-lg-6">
                     <div class="pie-chart chart block">
-                        <div class="title"><strong>{{ $sever->name }}'s player countries</strong></div>
+                        <div class="title"><strong>{{ $server->name }}'s player countries</strong></div>
                         <div class="radar-chart chart margin-bottom-sm">
                             <canvas id="serverCountries"></canvas>
                         </div>
@@ -58,13 +58,13 @@
             <div class="row">
                 <div class="col-lg-6">
                     <div class="line-chart block chart">
-                        <div class="title"><strong>{{ $sever->name }}'s online probability</strong></div>
+                        <div class="title"><strong>{{ $server->name }}'s online probability</strong></div>
                         <canvas id="onlineLineChartDays"></canvas>
                     </div>
                 </div>
                 <div class="col-lg-6">
                     <div class="line-chart block chart">
-                        <div class="title"><strong>{{ $sever->name }}'s online probability per day</strong></div>
+                        <div class="title"><strong>{{ $server->name }}'s online probability per day</strong></div>
                         <canvas id="onlineLineChartHours"></canvas>
                     </div>
                 </div>
