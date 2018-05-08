cd8c7dfc359f63d22eccdca2c1100be02d027919
Steffen Keuper <steffen.keuper@web.de>
2018-05-08 19:46:57 +0200

    [TASK] add console commands to update player and server data for the scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\UpdatePlayerData;
+use App\Console\Commands\UpdateServerData;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        UpdatePlayerData::class,
+        UpdateServerData::class
     ];
 
     /**
@@ -24,8 +27,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('data:servers')->everyFiveMinutes();
+        $schedule->command('data:players')->everyFiveMinutes();
     }
 
     /**
