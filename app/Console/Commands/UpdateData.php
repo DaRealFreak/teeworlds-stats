37272e52c2ff0b2b5f4b2d00f4838274397dcdcc
Steffen Keuper <steffen.keuper@web.de>
2018-05-10 23:31:17 +0200

    [TASK] implement data updating console command

--- app/Console/Commands/UpdateData.php
+++ app/Console/Commands/UpdateData.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Server;
 use App\TwRequest\TwRequest;
 use Illuminate\Console\Command;
 
@@ -44,12 +45,117 @@ class UpdateData extends Command
     {
         $this->twRequest->loadServersFromMasterservers();
         $this->twRequest->loadServerInfo();
-        ob_start();
         $this->twRequest->reloadFailedServerInfo();
-        var_export($this->twRequest->getServers());
-        file_put_contents("/app/dump_" . time() . ".txt", ob_get_flush());
+
+        $servers = $this->twRequest->getServers();
+
+        foreach ($servers as $server) {
+            $this->updateServer($server);
+            $this->updatePlayers($server);
+        }
 
         $this->info('update server data');
         return True;
+    }
+
+    private function updateServer($server)
+    {
+        /** @var Server $serverModel */
+        $serverModel = Server::firstOrCreate(
+            [
+                'ip' => $server[0],
+                'port' => $server[1]
+            ]
+        );
+        $serverModel->setAttribute('name', $server['name']);
+        $serverModel->setAttribute('version', $server['version']);
+        $serverModel->setAttribute('mod', $server['gametype']);
+
+        // if the server model only got created just now it doesn't have stats yet
+        if (!$serverModel->stats()->first()) {
+            $serverModel->stats()->create();
+        }
+
+        // check if the map already got tracked, create if not, else update the number of tracked times
+        $serverMapModel = $serverModel->maps->where('map', $server['map'])->first();
+        if (!$serverMapModel) {
+            $serverModel->maps()->create([
+                'map' => $server['map']
+            ]);
+        } else {
+            /** @var \App\Models\ServerMap $serverMapModel */
+            $serverMapModel->setAttribute('times', $serverMapModel->getAttribute('times') + 1);
+            $serverMapModel->save();
+        }
+
+        // save all changes to the model
+        $serverModel->save();
+    }
+
+    private function updatePlayers($server)
+    {
+        foreach ($server['players'] as $player) {
+            /** @var \App\Models\Player $playerModel */
+            $playerModel = \App\Models\Player::firstOrCreate(
+                [
+                    'name' => $player['name'],
+                ]
+            );
+
+            // create stats if no stats set yet
+            if (!$playerModel->stats()->first()) {
+                $playerModel->stats()->create();
+            }
+
+            // update player online stats
+            $currentHour = \Carbon\Carbon::now()->format('H');
+            $currentDay = strtolower(\Carbon\Carbon::now()->format('l'));
+            $playerModel->stats()->first()->update([
+                'hour_' . $currentHour => $playerModel->stats()->first()->getAttribute('hour_' . $currentHour) + 1,
+                $currentDay => $playerModel->stats()->first()->getAttribute($currentDay) + 1
+            ]);
+
+            // update player clan stat
+            // if clan is set and player has no clan or different clan
+            if ($player['clan'] && (!$playerModel->clan()->first() || $playerModel->clan()->first()->getAttribute('name') != $player['clan'])) {
+                /** @var \App\Models\Clan $clanModel */
+                $clanModel = \App\Models\Clan::firstOrCreate(
+                    [
+                        'name' => $player['clan'],
+                    ]
+                );
+                $playerModel->clan()->associate($clanModel);
+                $playerModel->setAttribute('clan_joined_at', \Carbon\Carbon::now());
+            } elseif (!$player['clan'] && $playerModel->clan()->first()) {
+                $playerModel->clan()->first()->delete();
+            }
+
+            // update player map stat
+            $playerMapModel = $playerModel->maps->where('map', $server['map'])->first();
+            if (!$playerMapModel) {
+                $playerModel->maps()->create([
+                    'map' => $server['map']
+                ]);
+            } else {
+                /** @var \App\Models\PlayerMap $playerMapModel */
+                $playerMapModel->setAttribute('times', $playerMapModel->getAttribute('times') + 1);
+                $playerMapModel->save();
+            }
+
+            // update player mod stat
+            $playerModModel = $playerModel->mods->where('mod', $server['gametype'])->first();
+            if (!$playerModModel) {
+                $playerModel->mods()->create([
+                    'mod' => $server['gametype']
+                ]);
+            } else {
+                /** @var \App\Models\PlayerMap $playerModModel */
+                $playerModModel->setAttribute('times', $playerModModel->getAttribute('times') + 1);
+                $playerModModel->save();
+            }
+
+            $playerModel->setAttribute('country', $this->twRequest::getCountryName($player['country']));
+            $playerModel->save();
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/search', 'SearchController@main')->name('search');
 # Navigation if logged in routes
 Route::get('/tee/edit/{tee_name}', 'InformationController@editPlayer')->name('editPlayer');
 Route::get('/clan/edit/{clan_name}', 'InformationController@editClan')->name('editClan');
-Route::get('/server/edit/{server_id}/{server_name}', 'InformationController@editServer')->name('editServer');
+Route::get('/server/edit/{clan_name}', 'InformationController@editServer')->name('editServer');
 
 # App specific routes
 Route::get('/tee', 'SearchController@searchTee')->name('tee');
@@ -30,32 +30,249 @@ Route::get('/clan', 'SearchController@searchClan')->name('clan');
 Route::get('/clan/{clan_name}/', ['as' => 'searchClanByName', 'uses' => 'SearchController@searchClanByName']);
 Route::get('/server', 'SearchController@searchServer')->name('server');
 Route::get('/server/{server_name}/', ['as' => 'searchServerByName', 'uses' => 'SearchController@searchServerByName']);
-Route::get('/server/{server_id}/{server_name}/', ['as' => 'searchServerByIdAndName', 'uses' => 'SearchController@searchServerByIdAndName']);
 
 # Authentication routes
 Auth::routes();
 
 # Test routes
 Route::get('/test', function () {
-    return "huge success";
-});
+    $server = [
+        0 => '194.67.208.6',
+        1 => 8319,
+        2 => 6,
+        'version' => '0.6.4, 11.1.5',
+        'name' => 'DDNet RUS - DDmaX [DDraceNetwork] [17/64]',
+        'map' => 'FWYS 4',
+        'gametype' => 'DDraceNetwork',
+        'password' => false,
+        'num_players' => 15,
+        'max_players' => 16,
+        'num_players_ingame' => 15,
+        'max_players_ingame' => 16,
+        'ping' => 145,
+        'players' =>
+            [
+                0 =>
+                    [
+                        'name' => 'brainless tee',
+                        'clan' => '',
+                        'country' => -1,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+                1 =>
+                    [
+                        'name' => 'GoldDigga',
+                        'clan' => 'gold medal',
+                        'country' => -1,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+                2 =>
+                    [
+                        'name' => 'RavQ',
+                        'clan' => 'FNG',
+                        'country' => 616,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+                3 =>
+                    [
+                        'name' => 'Orbit',
+                        'clan' => '',
+                        'country' => -1,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+                4 =>
+                    [
+                        'name' => 'Krasty',
+                        'clan' => 'FIXI',
+                        'country' => 643,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+                5 =>
+                    [
+                        'name' => 'PBAB',
+                        'clan' => '',
+                        'country' => -1,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+                6 =>
+                    [
+                        'name' => 'GoldMedal',
+                        'clan' => 'afk 5-10min',
+                        'country' => -1,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+                7 =>
+                    [
+                        'name' => 'Fiksiki',
+                        'clan' => 'Ihaveautism',
+                        'country' => -1,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+                8 =>
+                    [
+                        'name' => 'Eklektiko',
+                        'clan' => 'Pr0',
+                        'country' => -1,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+                9 =>
+                    [
+                        'name' => 'Thanos',
+                        'clan' => 'Rock',
+                        'country' => 50,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+                10 =>
+                    [
+                        'name' => ':0',
+                        'clan' => 'Saint d\'Arc',
+                        'country' => 520,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+                11 =>
+                    [
+                        'name' => 'The Direwolf',
+                        'clan' => 'jAGOD',
+                        'country' => 854,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+                12 =>
+                    [
+                        'name' => 'The Direfox',
+                        'clan' => '[DUMMY]',
+                        'country' => -1,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+                13 =>
+                    [
+                        'name' => 'tinky',
+                        'clan' => '',
+                        'country' => 643,
+                        'score' => -3187,
+                        'ingame' => true,
+                    ],
+                14 =>
+                    [
+                        'name' => 'pentagon!',
+                        'clan' => '',
+                        'country' => -1,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+                15 =>
+                    [
+                        'name' => 'syn',
+                        'clan' => 'Saint d\'Arc',
+                        'country' => 524,
+                        'score' => -9999,
+                        'ingame' => true,
+                    ],
+            ],
+    ];
 
-# Create example rules for player and server mods
-Route::get('/rules', function () {
-    \App\Models\ModRule::firstOrCreate(
+    /** @var \App\Models\Server $serverModel */
+    $serverModel = \App\Models\Server::firstOrCreate(
         [
-            'decider' => 'mod',
-            'rule' => '%fng%',
-            'mod_id' => \App\Models\Mod::firstOrCreate(['mod' => 'FNG'])->getAttribute('id')
+            'ip' => $server[0],
+            'port' => $server[1]
         ]
     );
+    $serverModel->setAttribute('name', $server['name']);
+    $serverModel->setAttribute('version', $server['version']);
+    $serverModel->setAttribute('mod', $server['gametype']);
+    if (!$serverModel->stats()->first()) {
+        $serverModel->stats()->create();
+    }
 
-    \App\Models\ModRule::firstOrCreate(
-        [
-            'decider' => 'server',
-            'rule' => '%gores%',
-            'mod_id' => \App\Models\Mod::firstOrCreate(['mod' => 'Gores'])->getAttribute('id')
-        ]
-    );
+    $serverMapModel = $serverModel->maps->where('map', $server['map'])->first();
+    if (!$serverMapModel) {
+        $serverModel->maps()->create([
+            'map' => $server['map']
+        ]);
+    } else {
+        /** @var \App\Models\ServerMap $serverMapModel */
+        $serverMapModel->setAttribute('times', $serverMapModel->getAttribute('times') + 1);
+        $serverMapModel->save();
+    }
+
+    foreach ($server['players'] as $player) {
+        /** @var \App\Models\Player $playerModel */
+        $playerModel = \App\Models\Player::firstOrCreate(
+            [
+                'name' => $player['name'],
+            ]
+        );
+
+        // create stats if no stats set yet
+        if (!$playerModel->stats()->first()) {
+            $playerModel->stats()->create();
+        }
+
+        // update player online stats
+        $currentHour = \Carbon\Carbon::now()->format('H');
+        $currentDay = strtolower(\Carbon\Carbon::now()->format('l'));
+        $playerModel->stats()->first()->update([
+            'hour_' . $currentHour => $playerModel->stats()->first()->getAttribute('hour_' . $currentHour) + 1,
+            $currentDay => $playerModel->stats()->first()->getAttribute($currentDay) + 1
+        ]);
+
+        // update player clan stat
+        // if clan is set and player has no clan or different clan
+        if ($player['clan'] && (!$playerModel->clan()->first() || $playerModel->clan()->first()->getAttribute('name') != $player['clan'])) {
+            /** @var \App\Models\Clan $clanModel */
+            $clanModel = \App\Models\Clan::firstOrCreate(
+                [
+                    'name' => $player['clan'],
+                ]
+            );
+            $playerModel->clan()->associate($clanModel);
+            $playerModel->setAttribute('clan_joined_at', \Carbon\Carbon::now());
+        } elseif (!$player['clan'] && $playerModel->clan()->first()) {
+            $playerModel->clan()->first()->delete();
+        }
+
+        // update player map stat
+        $playerMapModel = $playerModel->maps->where('map', $server['map'])->first();
+        if (!$playerMapModel) {
+            $playerModel->maps()->create([
+                'map' => $server['map']
+            ]);
+        } else {
+            /** @var \App\Models\PlayerMap $playerMapModel */
+            $playerMapModel->setAttribute('times', $playerMapModel->getAttribute('times') + 1);
+            $playerMapModel->save();
+        }
+
+        // update player mod stat
+        $playerModModel = $playerModel->mods->where('mod', $server['gametype'])->first();
+        if (!$playerModModel) {
+            $playerModel->mods()->create([
+                'mod' => $server['gametype']
+            ]);
+        } else {
+            /** @var \App\Models\PlayerMap $playerModModel */
+            $playerModModel->setAttribute('times', $playerModModel->getAttribute('times') + 1);
+            $playerModModel->save();
+        }
+
+        $playerModel->setAttribute('country', \App\TwRequest\TwRequest::getCountryName($player['country']));
+        $playerModel->save();
+    }
+
+    $serverModel->save();
+    return 'hello world';
 });
 
