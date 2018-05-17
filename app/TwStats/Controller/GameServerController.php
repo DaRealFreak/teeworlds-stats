5c80074d03e3f88ecf28d4e058238e24d28ae996
Steffen Keuper <steffen.keuper@web.de>
2018-05-18 01:01:28 +0200

    [TASK] use array_shift for generating the data, fix the occasional random packet on 64 legacy response, implement sleep duration

--- app/TwStats/Controller/GameServerController.php
+++ app/TwStats/Controller/GameServerController.php
@@ -29,28 +29,31 @@ class GameServerController
         socket_set_nonblock($sock);
 
 
+        $i = 0;
         /** @var GameServer $server */
         foreach ($servers as &$server) {
             NetworkController::send_packet($sock, NetworkController::PACKETS['SERVERBROWSE_GETINFO'], $server);
             NetworkController::send_packet($sock, NetworkController::PACKETS['SERVERBROWSE_GETINFO_64_LEGACY'], $server);
-        }
 
-        $durationWithoutResponse = NetworkController::CONNECTION_SLEEP_DURATION;
-        usleep(NetworkController::CONNECTION_SLEEP_DURATION * 1000);
+            if (++$i % self::SERVER_CHUNK_SIZE === 0 || $i >= count($servers)) {
+                $durationWithoutResponse = NetworkController::CONNECTION_SLEEP_DURATION;
+                usleep(NetworkController::CONNECTION_SLEEP_DURATION * 1000);
 
-        while (true) {
-            if (!NetworkController::receive_packet($sock, $servers, array(self::class, "processPacket"))) {
-                if ($durationWithoutResponse > NetworkController::CONNECTION_TIMEOUT) {
-                    // we didn't receive any packets in time and cancel the connection here
-                    break;
-                } else {
-                    // increase the measured duration without a response and sleep for the set duration
-                    $durationWithoutResponse += NetworkController::CONNECTION_SLEEP_DURATION;
-                    usleep(NetworkController::CONNECTION_SLEEP_DURATION * 1000);
+                while (true) {
+                    if (!NetworkController::receive_packet($sock, $servers, array(self::class, "processPacket"))) {
+                        if ($durationWithoutResponse > NetworkController::CONNECTION_TIMEOUT) {
+                            // we didn't receive any packets in time and cancel the connection here
+                            break;
+                        } else {
+                            // increase the measured duration without a response and sleep for the set duration
+                            $durationWithoutResponse += NetworkController::CONNECTION_SLEEP_DURATION;
+                            usleep(NetworkController::CONNECTION_SLEEP_DURATION * 1000);
+                        }
+                    } else {
+                        // if we got a response reset the duration in case we receive multiple packets
+                        $durationWithoutResponse = 0;
+                    }
                 }
-            } else {
-                // if we got a response reset the duration in case we receive multiple packets
-                $durationWithoutResponse = 0;
             }
         }
     }
@@ -74,7 +77,7 @@ class GameServerController
 
         // validate the server token
         $slots = explode("\x00", substr($data, 14, strlen($data) - 15));
-        $token = intval($slots[0]);
+        $token = intval(array_shift($slots));
         if (($token & 0xff) !== ord($server->getAttribute('_token'))) {
             // server token validation failed
             return;
@@ -84,26 +87,31 @@ class GameServerController
             case NetworkController::PACKETS['SERVERBROWSE_INFO']:
                 // vanilla
                 $server->setAttribute('server_type', 'vanilla');
-                $server->setAttribute('version', strval($slots[1]));
-                $server->setAttribute('name', strval($slots[2]));
-                $server->setAttribute('map', strval($slots[3]));
 
-                $server->setAttribute('gametype', strval($slots[4]));
-                $server->setAttribute('flags', intval($slots[5]));
-                $server->setAttribute('numplayers', intval($slots[6]));
-                $server->setAttribute('maxplayers', intval($slots[7]));
-                $server->setAttribute('numclients', intval($slots[8]));
-                $server->setAttribute('maxclients', intval($slots[9]));
+                $server->setAttribute('version', strval(array_shift($slots)));
+                $server->setAttribute('name', strval(array_shift($slots)));
+                $server->setAttribute('map', strval(array_shift($slots)));
+
+                $server->setAttribute('gametype', strval(array_shift($slots)));
+                $server->setAttribute('flags', intval(array_shift($slots)));
+                $server->setAttribute('numplayers', intval(array_shift($slots)));
+                $server->setAttribute('maxplayers', intval(array_shift($slots)));
+                $server->setAttribute('numclients', intval(array_shift($slots)));
+                $server->setAttribute('maxclients', intval(array_shift($slots)));
 
                 $players = $server->getAttribute('players');
-                for ($i = 10; $i < count($slots); $i += 5) {
-                    $players[] = Player::make([
-                        'name' => strval($slots[$i]),
-                        'clan' => strval($slots[$i + 1]),
-                        'country' => intval($slots[$i + 2]),
-                        'score' => intval($slots[$i + 3]),
-                        'ingame' => intval($slots[$i + 4]),
+                while (count($slots)) {
+                    /** @var Player $player */
+                    $player = Player::make([
+                        'name' => strval(array_shift($slots)),
+                        'clan' => strval(array_shift($slots)),
+                        'country' => intval(array_shift($slots)),
+                        'score' => intval(array_shift($slots)),
+                        'ingame' => intval(array_shift($slots)),
                     ]);
+                    if (!$server->doesPlayerAlreadyExist($player)) {
+                        $players[] = $player;
+                    }
                 }
                 $server->setAttribute('players', $players);
                 break;
@@ -113,26 +121,36 @@ class GameServerController
                     $server->setAttribute('server_type', '64_legacy');
                     $server->setAttribute('players', []);
                 }
-                $server->setAttribute('version', strval($slots[1]));
-                $server->setAttribute('name', strval($slots[2]));
-                $server->setAttribute('map', strval($slots[3]));
 
-                $server->setAttribute('gametype', strval($slots[4]));
-                $server->setAttribute('flags', intval($slots[5]));
-                $server->setAttribute('numplayers', intval($slots[6]));
-                $server->setAttribute('maxplayers', intval($slots[7]));
-                $server->setAttribute('numclients', intval($slots[8]));
-                $server->setAttribute('maxclients', intval($slots[9]));
+                $server->setAttribute('version', strval(array_shift($slots)));
+                $server->setAttribute('name', strval(array_shift($slots)));
+                $server->setAttribute('map', strval(array_shift($slots)));
+
+                $server->setAttribute('gametype', strval(array_shift($slots)));
+                $server->setAttribute('flags', intval(array_shift($slots)));
+                $server->setAttribute('numplayers', intval(array_shift($slots)));
+                $server->setAttribute('maxplayers', intval(array_shift($slots)));
+                $server->setAttribute('numclients', intval(array_shift($slots)));
+                $server->setAttribute('maxclients', intval(array_shift($slots)));
+
+                // i have no bloody idea what this packet is for
+                if ($slots[0] === '') {
+                    array_shift($slots);
+                }
 
                 $players = $server->getAttribute('players');
-                for ($i = 11; $i < count($slots); $i += 5) {
-                    $players[] = Player::make([
-                        'name' => strval($slots[$i]),
-                        'clan' => strval($slots[$i + 1]),
-                        'country' => intval($slots[$i + 2]),
-                        'score' => intval($slots[$i + 3]),
-                        'ingame' => intval($slots[$i + 4]),
+                while (count($slots)) {
+                    /** @var Player $player */
+                    $player = Player::make([
+                        'name' => strval(array_shift($slots)),
+                        'clan' => strval(array_shift($slots)),
+                        'country' => intval(array_shift($slots)),
+                        'score' => intval(array_shift($slots)),
+                        'ingame' => intval(array_shift($slots)),
                     ]);
+                    if (!$server->doesPlayerAlreadyExist($player)) {
+                        $players[] = $player;
+                    }
                 }
                 $server->setAttribute('players', $players);
                 break;
@@ -142,40 +160,45 @@ class GameServerController
                     $server->setAttribute('server_type', 'ext');
                     $server->setAttribute('players', []);
                 }
-                if (($slots[0] & 0xffff00) >> 8 !== ((ord($server->getAttribute('_request_token')[0]) << 8) + ord($server->getAttribute('_request_token')[1]))) {
+
+                if (($token & 0xffff00) >> 8 !== ((ord($server->getAttribute('_request_token')[0]) << 8) + ord($server->getAttribute('_request_token')[1]))) {
                     // additional server token validation failed
                     echo "request token validation failed";
                     return;
                 }
 
-                $server->setAttribute('version', strval($slots[1]));
-                $server->setAttribute('name', strval($slots[2]));
-                $server->setAttribute('map', strval($slots[3]));
+                $server->setAttribute('version', strval(array_shift($slots)));
+                $server->setAttribute('name', strval(array_shift($slots)));
+                $server->setAttribute('map', strval(array_shift($slots)));
 
-                $server->setAttribute('mapcrc', intval($slots[4]));
-                $server->setAttribute('mapsize', intval($slots[5]));
+                $server->setAttribute('mapcrc', intval(array_shift($slots)));
+                $server->setAttribute('mapsize', intval(array_shift($slots)));
 
-                $server->setAttribute('gametype', strval($slots[6]));
-                $server->setAttribute('flags', intval($slots[7]));
-                $server->setAttribute('numplayers', intval($slots[8]));
-                $server->setAttribute('maxplayers', intval($slots[9]));
-                $server->setAttribute('numclients', intval($slots[10]));
-                $server->setAttribute('maxclients', intval($slots[11]));
+                $server->setAttribute('gametype', strval(array_shift($slots)));
+                $server->setAttribute('flags', intval(array_shift($slots)));
+                $server->setAttribute('numplayers', intval(array_shift($slots)));
+                $server->setAttribute('maxplayers', intval(array_shift($slots)));
+                $server->setAttribute('numclients', intval(array_shift($slots)));
+                $server->setAttribute('maxclients', intval(array_shift($slots)));
 
                 $players = $server->getAttribute('players');
-                for ($i = 13; $i < count($slots); $i += 6) {
-                    $players[] = Player::make([
-                        'name' => strval($slots[$i]),
-                        'clan' => strval($slots[$i + 1]),
-                        'country' => intval($slots[$i + 2]),
-                        'score' => intval($slots[$i + 3]),
-                        'ingame' => intval($slots[$i + 4]),
-                        'unknown' => intval($slots[$i + 5]),
+                while (count($slots)) {
+                    /** @var Player $player */
+                    $player = Player::make([
+                        'name' => strval(array_shift($slots)),
+                        'clan' => strval(array_shift($slots)),
+                        'country' => intval(array_shift($slots)),
+                        'score' => intval(array_shift($slots)),
+                        'ingame' => intval(array_shift($slots)),
                     ]);
+                    // no idea what this is for, in every test case it was 0 but we gotta shift it too
+                    array_shift($slots);
+
+                    if (!$server->doesPlayerAlreadyExist($player)) {
+                        $players[] = $player;
+                    }
                 }
                 $server->setAttribute('players', $players);
-
-                var_dump($server->getAttributes());
                 break;
             case
             NetworkController::PACKETS['SERVERBROWSE_INFO_EXTENDED_MORE']:
