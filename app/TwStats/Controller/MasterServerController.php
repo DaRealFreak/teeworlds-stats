2747874170289565b5ac0f799db3a1f1f7363988
Steffen Keuper <steffen.keuper@web.de>
2018-05-17 04:08:35 +0200

    [TASK] use the models, validate the token of the request

--- app/TwStats/Controller/MasterServerController.php
+++ app/TwStats/Controller/MasterServerController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\TwStats\Controller;
+use App\TwStats\Models\GameServer;
+use App\TwStats\Models\MasterServer;
 
 /**
  * Class MasterServerController
@@ -10,7 +12,7 @@ class MasterServerController
 {
 
     /** @var array teeworlds master servers */
-    protected static $masterservers = [
+    protected static $masterServers = [
         [
             "hostname" => "master1.teeworlds.com",
             "port" => 8300
@@ -29,11 +31,22 @@ class MasterServerController
         ]
     ];
 
+    public static function getMasterServers()
+    {
+        foreach (self::$masterServers as $masterServer) {
+            yield MasterServer::make([
+                'hostname' => $masterServer['hostname'],
+                'ip' => gethostbyname($masterServer['hostname']),
+                'port' => $masterServer['port'],
+            ]);
+        }
+    }
+
     /**
      * check the master servers for the game server count
      * and retrieve the server list
      *
-     * @return array
+     * @return MasterServer[]
      */
     public static function getServers()
     {
@@ -42,14 +55,10 @@ class MasterServerController
         // set the socket to non blocking to allow parallel requests
         socket_set_nonblock($sock);
 
-        foreach (self::$masterservers as &$server) {
-            // if we are using DNS entries resolve the ip to handle the response properly
-            if (!isset($server['ip'])) {
-                $server['ip'] = gethostbyname($server['hostname']);
-            }
+        $masterServers = iterator_to_array(self::getMasterServers());
 
-            $server['response'] = false;
-
+        /** @var MasterServer $server */
+        foreach ($masterServers as &$server) {
             NetworkController::send_packet($sock, NetworkController::PACKETS['SERVERBROWSE_GETCOUNT'], $server);
             NetworkController::send_packet($sock, NetworkController::PACKETS['SERVERBROWSE_GETLIST'], $server);
         }
@@ -58,7 +67,7 @@ class MasterServerController
         usleep(NetworkController::CONNECTION_SLEEP_DURATION * 1000);
 
         while (true) {
-            if (!NetworkController::receive_packet($sock, self::$masterservers, array(self::class, "processPacket"))) {
+            if (!NetworkController::receive_packet($sock, $masterServers, array(self::class, "processPacket"))) {
                 if ($durationWithoutResponse > NetworkController::CONNECTION_TIMEOUT) {
                     // we didn't receive any packets in time and cancel the connection here
                     socket_close($sock);
@@ -74,28 +83,42 @@ class MasterServerController
             }
         }
 
-        return self::$masterservers;
+        return $masterServers;
     }
 
     /**
-     * process packet for SERVERBROWSE_COUNT & SERVERBROWSE_LIST packets
+     * process packet for
+     *
+     * SERVERBROWSE_COUNT
+     * SERVERBROWSE_LIST
+     *
+     * packets
      *
      * @param $data
      * @param $server
      */
-    public static function processPacket($data, &$server)
+    public static function processPacket($data, MasterServer &$server)
     {
-        $server['response'] = true;
+        $server->setAttribute('response', true);
+
+        // validate the server token
+        $token = intval(explode("\x00", substr($data, 14, strlen($data) - 15))[0]);
+        if (($token & 0xff) !== ord($server->getAttribute('_token'))) {
+            // server token validation failed
+            exit;
+        }
 
         switch (substr($data, 6, 8)) {
 
             // we are receiving the information how many game servers we get from this master server
             case NetworkController::PACKETS['SERVERBROWSE_COUNT']:
-                $server['num_servers'] = ((ord($data[14]) << 8) | ord($data[15]));
+                $server->setAttribute('num_servers', ((ord($data[14]) << 8) | ord($data[15])));
                 break;
 
             // we are receiving the information of the game servers
             case NetworkController::PACKETS['SERVERBROWSE_LIST']:
+                /** @var GameServer[] $currentGameServers */
+                $currentGameServers = $server->getAttribute('servers');
                 for ($i = 14; ($i + 18) <= strlen($data); $i += 18) {
 
                     /* switch between IPv4 and IPv6 */
@@ -106,8 +129,12 @@ class MasterServerController
                     }
 
                     $port = hexdec(bin2hex(substr($data, $i + 16, 2)));
-                    $server['servers'][] = ['ip' => $ip, 'port' => $port];
+                    $currentGameServers[] = GameServer::make([
+                        'ip' => $ip,
+                        'port' => $port
+                    ]);
                 }
+                $server->setAttribute('servers', $currentGameServers);
                 break;
 
             // unknown packet
