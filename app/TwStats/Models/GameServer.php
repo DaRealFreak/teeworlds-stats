33570926d03d6588cf08b9903023edc53424a99e
Steffen Keuper <steffen.keuper@web.de>
2018-05-17 04:06:21 +0200

    [TASK] add models for master and game servers

---
+++ app/TwStats/Models/GameServer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\TwStats\Models;
+
+/**
+ * App\TwStats\Models\GameServer
+ *
+ * @mixin \Eloquent
+ */
+class GameServer extends Server
+{
+    protected $additionalAttributes = [
+        'port' => 8303,
+        'players' => []
+    ];
+
+    protected $additionalFillables = [
+        'servers', 'num_servers'
+    ];
+
+    public function __construct(array $attributes = [])
+    {
+        $this->attributes = array_merge(parent::getAttributes(), $this->additionalAttributes);
+        $this->fillable = array_merge(parent::getFillable(), $this->additionalFillables);
+        parent::__construct($attributes);
+    }
+}
