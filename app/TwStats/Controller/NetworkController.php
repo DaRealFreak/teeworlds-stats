8600c3489cf60cc49e5d8ffce542fd681e20a4a0
Steffen Keuper <steffen.keuper@web.de>
2018-05-17 04:07:55 +0200

    [TASK] add packet structs, implement additional token for game server requests, catch errors on refused connection

--- app/TwStats/Controller/NetworkController.php
+++ app/TwStats/Controller/NetworkController.php
@@ -2,6 +2,9 @@
 
 namespace App\TwStats\Controller;
 
+use App\TwStats\Models\GameServer;
+use App\TwStats\Models\Server;
+
 
 /**
  * Class NetworkController
@@ -19,7 +22,13 @@ class NetworkController
         'SERVERBROWSE_GETCOUNT' => "\xff\xff\xff\xffcou2",
         'SERVERBROWSE_COUNT' => "\xff\xff\xff\xffsiz2",
         'SERVERBROWSE_GETLIST' => "\xff\xff\xff\xffreq2",
-        'SERVERBROWSE_LIST' => "\xff\xff\xff\xfflis2"
+        'SERVERBROWSE_LIST' => "\xff\xff\xff\xfflis2",
+        'SERVERBROWSE_GETINFO_64_LEGACY' => "\xff\xff\xff\xfffstd",
+        'SERVERBROWSE_INFO_64_LEGACY' => "\xff\xff\xff\xffdtsf",
+        'SERVERBROWSE_GETINFO' => "\xff\xff\xff\xffgie3",
+        'SERVERBROWSE_INFO' => "\xff\xff\xff\xffinf3",
+        'SERVERBROWSE_INFO_EXTENDED' => "\xff\xff\xff\xffiext",
+        'SERVERBROWSE_INFO_EXTENDED_MORE' => "\xff\xff\xff\xffiex+",
     ];
 
     /**
@@ -29,17 +38,33 @@ class NetworkController
      * @param $sock
      * @param $data
      * @param $server
+     * @return bool
      */
-    public static function send_packet($sock, $data, &$server)
+    public static function send_packet($sock, $data, Server &$server)
     {
         // generate 2 random bytes as server request token
-        if (!isset($server['_request_token'])) {
-            $server['_request_token'] = random_bytes(2);
+        if (!$server->getAttribute('_request_token')) {
+            $server->setAttribute('_request_token', random_bytes(2));
         }
 
-        $packet = sprintf("xe%s\0\0%s", $server['_request_token'], $data);
+        $packet = sprintf("xe%s\0\0%s", $server->getAttribute('_request_token'), $data);
 
-        socket_sendto($sock, $packet, strlen($packet), 0, $server['hostname'], $server['port']);
+        // add additional token for game server requests
+        if ($server instanceof GameServer) {
+            if (!$server->getAttribute('_token')) {
+                $server->setAttribute('_token', random_bytes(1));
+            }
+            $packet .= $server->getAttribute('_token');
+        }
+
+        try {
+            socket_sendto($sock, $packet, strlen($packet), 0, $server->getAttribute('ip'), $server->getAttribute('port'));
+            return True;
+        } /** @noinspection PhpRedundantCatchClauseInspection */
+        catch (\ErrorException $err) {
+            // server not reachable or we got blocked
+            return False;
+        }
     }
 
     /**
@@ -48,18 +73,22 @@ class NetworkController
      * and pass the server together with the data to the processing function given as a callback
      *
      * @param $sock
-     * @param $servers
+     * @param Server[] $servers
      * @param callable $processingCallback
      * @return bool
      */
-    public static function receive_packet($sock, &$servers, callable $processingCallback)
+    public static function receive_packet($sock, array &$servers, callable $processingCallback)
     {
-        if (!socket_recvfrom($sock, $data, 2048, 0, $ip, $port)) {
+        try {
+            if (!socket_recvfrom($sock, $data, 2048, 0, $ip, $port)) {
+                return false;
+            }
+        } catch (\Exception $err) {
             return false;
         }
 
         foreach ($servers as &$server) {
-            if ($server['ip'] == $ip && $server['port'] == $port) {
+            if ($server->getAttribute('ip') == $ip && $server->getAttribute('port') == $port) {
                 $processingCallback($data, $server);
             }
         }
