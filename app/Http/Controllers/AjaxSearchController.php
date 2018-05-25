bdd94ee728ba546066b793c7e8e617744c58844c
Steffen Keuper <steffen.keuper@web.de>
2018-05-25 05:33:37 +0200

    [TASK] add ajax search controller to use jquery-ui autocomplete with and register routes

---
+++ app/Http/Controllers/AjaxSearchController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Clan;
+use App\Models\Player;
+use App\Models\Server;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Str;
+
+class AjaxSearchController extends Controller
+{
+
+    /**
+     * @param Request $request
+     * @return string
+     */
+    public function searchTee(Request $request)
+    {
+        $term = Str::lower(Input::get('term'));
+
+        $playerSuggestions = Player::where('name', 'like', '%' . $term . '%')
+            ->orderByRaw('`name` LIKE ? DESC', $term . '%')
+            ->orderBy('name')
+            ->limit(10)
+            ->get();
+
+        $suggestions = new Collection();
+        foreach ($playerSuggestions as $suggestion) {
+            $suggestions->add($suggestion->name);
+        }
+
+        return $suggestions->toJson();
+    }
+
+    /**
+     * @param Request $request
+     * @return string
+     */
+    public function searchClan(Request $request)
+    {
+        $term = Str::lower(Input::get('term'));
+
+        $clanSuggestions = Clan::where('name', 'like', '%' . $term . '%')
+            ->orderByRaw('`name` LIKE ? DESC', $term . '%')
+            ->orderBy('name')
+            ->limit(10)
+            ->get();
+
+        $suggestions = new Collection();
+        foreach ($clanSuggestions as $suggestion) {
+            $suggestions->add($suggestion->name);
+        }
+
+        return $suggestions->toJson();
+    }
+
+    /**
+     * @param Request $request
+     * @return string
+     */
+    public function searchServer(Request $request)
+    {
+        $term = Str::lower(Input::get('term'));
+
+        $serverSuggestions = Server::where('name', 'like', '%' . $term . '%')
+            ->orderByRaw('`name` LIKE ? DESC', $term . '%')
+            ->orderBy('name')
+            ->limit(10)
+            ->get();
+
+        $suggestions = new Collection();
+        foreach ($serverSuggestions as $suggestion) {
+            $suggestions->add($suggestion->name);
+        }
+
+        return $suggestions->toJson();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -26,14 +26,16 @@ Route::get('/server/edit/{server_id}/{server_name}', 'InformationController@edit
 # App specific routes
 Route::get('/tee', 'SearchController@searchTee')->name('tee');
 Route::get('/tee/{tee_name}/', ['as' => 'searchTeeByName', 'uses' => 'SearchController@searchTeeByName']);
-Route::get('/tee/search/{tee_name}/', ['as' => 'searchTeeByNameAjax', 'uses' => 'SearchController@searchTeeByNameAjax']);
 Route::get('/clan', 'SearchController@searchClan')->name('clan');
 Route::get('/clan/{clan_name}/', ['as' => 'searchClanByName', 'uses' => 'SearchController@searchClanByName']);
-Route::get('/clan/search/{clan_name}/', ['as' => 'searchClanByNameAjax', 'uses' => 'SearchController@searchClanByNameAjax']);
 Route::get('/server', 'SearchController@searchServer')->name('server');
 Route::get('/server/{server_name}/', ['as' => 'searchServerByName', 'uses' => 'SearchController@searchServerByName']);
 Route::get('/server/{server_id}/{server_name}/', ['as' => 'searchServerByIdAndName', 'uses' => 'SearchController@searchServerByIdAndName']);
-Route::get('/server/search/{server_name}/', ['as' => 'searchServerByNameAjax', 'uses' => 'SearchController@searchServerByNameAjax']);
+
+# Ajax search routes
+Route::get('/search/tee', 'AjaxSearchController@searchTee')->name('searchTeeAjax');
+Route::get('/search/clan', 'AjaxSearchController@searchClan')->name('searchClanAjax');
+Route::get('/search/server', 'AjaxSearchController@searchServer')->name('searchServerAjax');
 
 # Authentication routes
 Auth::routes();
